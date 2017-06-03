3aa6b647067c64b1bda6d9c827fb531fbadd4932
坂本のぞみ <sakamotonozomi@M-Nozomi-Sakamoto-noMacBookAir.local>
2017-06-03 16:24:53 +0900

    始め

---
+++ SwimeeWorkShop/TimeLineViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TimeLineViewController.swift
+//  SwimeeWorkShop
+//
+//  Created by 坂本のぞみ on 2017/06/03.
+//  Copyright © 2017年 Yuki Takei. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    var myCollectionView:UICollectionView!
+    
+        let photos = ["a","b","c","d","e","f","g","h","i","j"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) ->UICollectionViewCell{
+        
+        // Cell はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
+        
+        // Tag番号を使ってImageViewのインスタンス生成
+        let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
+        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
+        let cellImage = UIImage(named: photos[(indexPath as NSIndexPath).row])
+        // UIImageをUIImageViewのimageとして設定
+        imageView.image = cellImage
+        
+        return testCell
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // section数は１つ
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
+        return 10;
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    }
